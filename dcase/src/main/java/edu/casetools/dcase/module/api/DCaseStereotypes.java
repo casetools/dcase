852bef1ce4cefd7d238b23f890e163a813c1f2b3
Unai Alegre <u.alegre@mdx.ac.uk>
2016-11-15 16:23:09 +0000

    Added new elements required for translating to M. Reorganized the diagrams menu.

--- dcase/src/main/java/edu/casetools/dcase/module/api/DCaseStereotypes.java
+++ dcase/src/main/java/edu/casetools/dcase/module/api/DCaseStereotypes.java
@@ -2,11 +2,17 @@ package edu.casetools.dcase.module.api;
 
 public class DCaseStereotypes { // NOSONAR The class only contains static
 				// Strings.
+    public static final String STEREOTYPE_ANTECEDENT_GROUP = "AntecedentGroupStereotype";
+    public static final String STEREOTYPE_ANTECEDENT = "AntecedentStereotype";
+    public static final String STEREOTYPE_CONSEQUENT = "ConsequentStereotype";
+    public static final String STEREOTYPE_STATE = "StateStereotype";
 
     public static final String STEREOTYPE_MESSAGE = "ContextInformationMessageStereotype";
     public static final String STEREOTYPE_DIAGRAM_INTERACTION = "InteractionDiagramStereotype";
     public static final String STEREOTYPE_DIAGRAM_COMMUNICATION = "CommunicationDiagramStereotype";
     public static final String STEREOTYPE_DEPENDENCY_PRODUCE = "ProduceStereotype";
     public static final String STEREOTYPE_DEPENDENCY_CONSUME = "ConsumeStereotype";
-    public static final String STEREOTYPE_DIAGRAM_CONTEXT_RULES = "ContextRulesDiagramStereotype";
+    public static final String STEREOTYPE_DIAGRAM_CSPARQL_RULES = "CSPARQLRulesDiagramStereotype";
+    public static final String STEREOTYPE_DIAGRAM_M_RULES = "MRulesDiagramStereotype";
+
 }
