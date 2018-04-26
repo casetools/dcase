1fd7070e03cd807897561149ed98ec999a0608df
Unai <u.alegre@mdx.ac.uk>
2018-04-26 02:20:35 +0100

    Minor fixes on the tools for the acquisition and modelling diagram

--- dcase/src/main/java/edu/casetools/dcase/modelio/diagrams/acqmod/tools/elements/DBModellingRuleTool.java
+++ dcase/src/main/java/edu/casetools/dcase/modelio/diagrams/acqmod/tools/elements/DBModellingRuleTool.java
@@ -25,12 +25,15 @@ import java.util.List;
 import org.modelio.api.modelio.diagram.IDiagramGraphic;
 import org.modelio.api.modelio.diagram.IDiagramNode;
 import org.modelio.api.modelio.model.IModelingSession;
+import org.modelio.metamodel.mmextensions.infrastructure.ExtensionNotFoundException;
+import org.modelio.metamodel.uml.statik.Class;
 import org.modelio.vcore.smkernel.mapi.MObject;
 
 import edu.casetools.dcase.module.api.DCaseColours;
 import edu.casetools.dcase.module.api.DCaseStereotypes;
 import edu.casetools.dcase.module.i18n.I18nMessageService;
 import edu.casetools.dcase.module.impl.DCaseModule;
+import edu.casetools.dcase.module.impl.DCasePeerModule;
 import edu.casetools.rcase.modelio.diagrams.ElementTool;
 import edu.casetools.rcase.module.api.RCaseColours;
 import edu.casetools.rcase.utils.ElementUtils;
@@ -49,10 +52,11 @@ public class DBModellingRuleTool extends ElementTool {
      */
     @Override
     public MObject createOwnElement(IModelingSession session, MObject element) {
-	String name = I18nMessageService.getString("Names.MobileSensor");
+	String name = I18nMessageService.getString("Names.DBModellingRule");
 	
-	return ElementUtils.getInstance().createClass(DCaseModule.getInstance(), adaptElement(element), session, name,
-			DCaseStereotypes.STEREOTYPE_MOBILE_SENSOR);
+	Class auxiliarElement = ElementUtils.getInstance().createClass(DCaseModule.getInstance(), adaptElement(element), session, name,
+			DCaseStereotypes.STEREOTYPE_MODELLING_RULE);
+		return addStereotype(auxiliarElement);
     }
 
     /*
@@ -76,5 +80,15 @@ public class DBModellingRuleTool extends ElementTool {
 
 	return graph;
     }
+    
+	private Class addStereotype(Class object) {
+		try {
+			object.addStereotype(DCasePeerModule.MODULE_NAME, DCaseStereotypes.STEREOTYPE_DB_MODELLING_RULE);
+		} catch (ExtensionNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return object;
+	}
    
 }
--- dcase/src/main/java/edu/casetools/dcase/modelio/diagrams/acqmod/tools/elements/StationarySensorTool.java
+++ dcase/src/main/java/edu/casetools/dcase/modelio/diagrams/acqmod/tools/elements/StationarySensorTool.java
@@ -25,12 +25,15 @@ import java.util.List;
 import org.modelio.api.modelio.diagram.IDiagramGraphic;
 import org.modelio.api.modelio.diagram.IDiagramNode;
 import org.modelio.api.modelio.model.IModelingSession;
+import org.modelio.metamodel.mmextensions.infrastructure.ExtensionNotFoundException;
+import org.modelio.metamodel.uml.statik.Class;
 import org.modelio.vcore.smkernel.mapi.MObject;
 
 import edu.casetools.dcase.module.api.DCaseColours;
 import edu.casetools.dcase.module.api.DCaseStereotypes;
 import edu.casetools.dcase.module.i18n.I18nMessageService;
 import edu.casetools.dcase.module.impl.DCaseModule;
+import edu.casetools.dcase.module.impl.DCasePeerModule;
 import edu.casetools.rcase.modelio.diagrams.ElementTool;
 import edu.casetools.rcase.module.api.RCaseColours;
 import edu.casetools.rcase.utils.ElementUtils;
@@ -51,8 +54,9 @@ public class StationarySensorTool extends ElementTool {
     public MObject createOwnElement(IModelingSession session, MObject element) {
 	String name = I18nMessageService.getString("Names.StationarySensor");
 	
-	return ElementUtils.getInstance().createClass(DCaseModule.getInstance(), adaptElement(element), session, name,
-			DCaseStereotypes.STEREOTYPE_STATIONARY_SENSOR);
+	Class auxiliarElement = ElementUtils.getInstance().createClass(DCaseModule.getInstance(), adaptElement(element), session, name,
+			DCaseStereotypes.STEREOTYPE_SENSOR);
+		return addStereotype(auxiliarElement);
     }
 
     /*
@@ -76,5 +80,16 @@ public class StationarySensorTool extends ElementTool {
 
 	return graph;
     }
+    
+	private Class addStereotype(Class object) {
+		try {
+			object.addStereotype(DCasePeerModule.MODULE_NAME, DCaseStereotypes.STEREOTYPE_STATIONARY_SENSOR);
+		} catch (ExtensionNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return object;
+	}
+   
    
 }
